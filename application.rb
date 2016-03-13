c834d04b6ddd3b21686ece4698ccfe6ccb095fe9
Colin Stannard <colin.stannard@cbsindustries.com>
2016-03-13 11:22:26 +0000

    Update application authentication to live mode.

--- application.rb
+++ application.rb
@@ -1,10 +1,12 @@
 require 'zara4'
 
-Zara4::API::Communication::Config::enter_development_mode()
 
+#
+# Obtain your API credentials from https://zara4.com/account/api-clients/live-credentials
+#
 client = Zara4::API::Client.new({
-  'client_id'     => '9WQcfHDyd8BIJc9tg2Yx6qSjHn8bvKyPbFs',
-  'client_secret' => 'Ua25inHBHWWwExMb4Xymb9k7K9l8f2I2X4h'
+  'client_id'     => 'YOUR_API_CLIENT_ID',
+  'client_secret' => 'YOUR_API_CLIENT_SECRET'
 })
 
 
